d1d12c9cdf39feee95d3ddfcd4149f6facda5a20
mena0018 <rabie.menad@etudiant.univ-reims.fr>
2022-04-22 12:38:13 +0200

    Ajout de l'animation lorsque le serpent avance

--- public/js/app.js
+++ public/js/app.js
@@ -1,26 +1,60 @@
-const canvas = document.getElementById('canvas');
-const ctx = canvas.getContext('2d');
+const canvas = document.getElementById("canvas");
+const ctx = canvas.getContext("2d");
 
-ctx.fillStyle = "#ececec";
-ctx.strokeStyle= "black";
-ctx.fillRect(0, 0, canvas.width, canvas.height);
-ctx.strokeRect(0, 0, canvas.width, canvas.height);
+// Vitesse sur X
+vx = 10;
 
-let snake = [ {x:140, y:150}, {x:130, y:150}, {x:120, y:150}, {x:110, y:150} ]
+// Vitesse sur Y
+vy = -0;
+
+let snake = [
+  { x: 140, y: 150 },
+  { x: 130, y: 150 },
+  { x: 120, y: 150 },
+  { x: 110, y: 150 },
+];
+
+function animation() {
+  setTimeout(function () {
+    nettoieCanvas();
+    faireAvancerSerpent();
+    dessineLeSerpent();
+    animation();
+  }, 100);
+}
+animation();
+
+
+function nettoieCanvas() {
+  ctx.fillStyle = "#ececec";
+  ctx.strokeStyle = "black";
+  ctx.fillRect(0, 0, canvas.width, canvas.height);
+  ctx.strokeRect(0, 0, canvas.width, canvas.height);
+}
 
 
 function dessineLesMorceaux(morceau) {
-    ctx.fillStyle = "#00fe14";
-    ctx.strokeStyle = "black";
-    ctx.fillRect(morceau.x, morceau.y, 10, 10);
-    ctx.strokeRect(morceau.x, morceau.y, 10, 10);
+  ctx.fillStyle = "#00fe14";
+  ctx.strokeStyle = "black";
+  ctx.fillRect(morceau.x, morceau.y, 10, 10);
+  ctx.strokeRect(morceau.x, morceau.y, 10, 10);
 }
 
 
 function dessineLeSerpent() {
-    snake.forEach(morceau => {
-        dessineLesMorceaux(morceau)
-    })
+  snake.forEach((morceau) => {
+    dessineLesMorceaux(morceau);
+  });
 }
 
-dessineLeSerpent()
+
+function faireAvancerSerpent() {
+  const head = {
+    x: snake[0].x + vx,
+    y: snake[0].y + vy,
+  };
+  snake.unshift(head);
+  snake.pop();
+}
+dessineLeSerpent();
+
